524c0df8eedef15399f0eee4274b04c5e6677231
Hannah Kuo <hannahkuo0205@gmail.com>
2021-07-13 18:27:21 -0700

    Cleaned up App.js. Added books description (alignment is off).

--- App.js
+++ App.js
@@ -8,28 +8,6 @@ import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
 
-	// const [books, setBooks] = useState([])
-	// console.log('test');
-	// useEffect(() => {
-	// 	// const fetchBooks = async () => {
-	// 	// 	const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=BB8BqGACHA9HPxtAblxoX55uesGNfYKj`)
-	// 	// 	console.log(response.data);
-	// 	// 	setBooks(response.data.results.books);
-
-	// 	// }
-	// 	// fetchBooks();
-
-	// 	const fetchBooks = async function fetchBooks() {
-	// 		const response = await fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=BB8BqGACHA9HPxtAblxoX55uesGNfYKj');
-	// 		const books = await response.json();
-	// 		console.log(response.data);
-	// 		setBooks(books.results);
-	// 		return books.results
-	// 	}
-	// 	fetchBooks();
-
-
-	// }, [])
 
 	const [books, setBooks] = useState([{}])
 
@@ -56,50 +34,31 @@ export default function App() {
 	console.log(books)
 	
 
-
-	// const App = () => {
-	// 	const API_KEY = "BB8BqGACHA9HPxtAblxoX55uesGNfYKj"
-	// 	const URL = `https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${API_KEY}`
-
-	// 	const [books, setBooks] = useState([]);
-	// 	useEffect(() => {
-	// 		fetch(URL)
-	// 		.then((response) => response.json())
-	// 		.then((responseJson) => {
-	// 			return responseJson.books;
-	// 		})
-	// 		.then(books => {
-	// 			setBooks(books);
-	// 			console.log(books);
-	// 		})
-	// 		.catch(error => {
-	// 			console.log(error);
-	// 		})
-	// 	},[])
-		
-	// }
-
-
-	// const [books, setBooks] = useState([
-	// 	{rank: '1', title: 'book one'},
-	// 	{rank: '2', title: 'book two'},
-	// ]);
-
-
 	return (
 		<View style={styles.container}>
 
 			<Text>Weekly New York Times Bestselling Books Top 15:</Text>
 
 			{books.map((book) => {
-				const {age_group, author, book_image, buy_links, description, price, primary_isbn10, publisher, rank, title} = book
+				const {
+					author, 
+					book_image, 
+					buy_links, 
+					description, 
+					price, 
+					primary_isbn10, 
+					publisher, 
+					rank, 
+					title, 
+					rank_last_week, 
+					weeks_on_list} = book
 				// const { rank, title } = book
 
 				return (
 
 					<View key={rank}>
 						<Text>{book.title}</Text>
-
+						<Text>{book.description}</Text>
 						<Image
 							style={styles.bookCover}
 							source={{uri: book.book_image}}
